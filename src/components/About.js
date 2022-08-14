0f9f534442dc3c867d90701387a5e494af90c239
aniket-sen-1 <aniketsen570@gmail.com>
2022-08-15 00:54:45 +0530

    home image added in readme

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from "react";
 
-function About(){
-  return(
-    <div className='about'>
-    <div className='about--heading'>
-      About
+function About() {
+  return (
+    <div className="about">
+      <div className="about--heading">About</div>
+      <div className="about--text">
+        I am a frontend developer with a particular interest in making things
+        simple and automating daily tasks. I try to keep up with security and
+        best practices, and am always looking for new things to learn.
       </div>
-    <div className='about--text'>
-      I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.
-      </div>
-      </div>
-    )
+    </div>
+  );
 }
 
 export default About;
